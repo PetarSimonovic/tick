520f1584747f3dfeb5a0f59610e7313f331d17ab
Pete Simonovic <69108995+PetarSimonovic@users.noreply.github.com>
2025-05-28 10:40:57 +0100

    Basic iteration working

--- src/logic/gameOfLife/gameOfLife.test.ts
+++ src/logic/gameOfLife/gameOfLife.test.ts
@@ -2,9 +2,18 @@ import { computeNextGeneration } from "./gameOfLife";
 import type { Seed } from "./gameOfLife.types";
 
 describe("computeNextGeneration", () => {
-  it("calculates underpopulation conditions for a single row with one cell", () => {
-    const seed: Seed = [[0, 1, 0]];
-    const result: Seed = computeNextGeneration(seed);
-    expect(result).toEqual([[0, 0, 0]]);
+  describe("underpopulation", () => {
+    // Rule: Any live cell with fewer than two live neighbours dies
+    it("calculates underpopulation conditions for a single row with one cell: no live neighbours", () => {
+      const seed: Seed = [[0, 1, 0]];
+      const result: Seed = computeNextGeneration(seed);
+      console.log(result);
+      expect(result).toEqual([[0, 0, 0]]);
+    });
+    it("calculates underpopulation conditions for a single row with one cell: two live neighbours", () => {
+      const seed: Seed = [[1, 1, 1]];
+      const result: Seed = computeNextGeneration(seed);
+      expect(result).toEqual([[0, 1, 0]]);
+    });
   });
 });
--- src/logic/gameOfLife/gameOfLife.ts
+++ src/logic/gameOfLife/gameOfLife.ts
@@ -1,5 +1,47 @@
 import type { Seed } from "./gameOfLife.types";
 
+const Neighbours: number[][] = [
+  [-1, -1], // top left
+  [-1, 0], // top
+  [-1, 1], // top right
+  [0, -1], // left
+  [0, 1], // right
+  [1, -1], // bottom left
+  [1, 0], // bottom
+  [1, 1], // bottom right
+];
+
 export const computeNextGeneration = (seed: Seed) => {
-  return [[0, 0, 0]];
+  return seed.map((row: number[], rowIndex: number) => {
+    return row.map((cell, colIndex) => {
+      const liveNeighbours: number = Neighbours.reduce(
+        (numberOfLiveNeighbours, [dx, dy]) => {
+          const neighbourRow: number = rowIndex + dx;
+          const neighbourCol = colIndex + dy;
+          // Check bounds
+          if (inGridBounds(neighbourRow, neighbourCol, seed)) {
+            return numberOfLiveNeighbours + seed[neighbourRow][neighbourCol];
+          }
+          return numberOfLiveNeighbours;
+        },
+        0
+      );
+      // For now, just return the count for demonstration
+      cell = liveNeighbours < 2 ? 0 : 1;
+      return cell;
+    });
+  });
+};
+
+const inGridBounds = (
+  neighbourRow: number,
+  neighbourCol: number,
+  seed: number[][]
+) => {
+  return (
+    neighbourRow >= 0 &&
+    neighbourRow < seed.length &&
+    neighbourCol >= 0 &&
+    neighbourCol < seed[0].length
+  );
 };
