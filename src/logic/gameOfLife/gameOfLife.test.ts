ca03cd22d1091d82fe1fe30c8f87bd8634cc795f
Pete Simonovic <69108995+PetarSimonovic@users.noreply.github.com>
2025-05-28 10:01:45 +0100

    Set up Seed type

--- src/logic/gameOfLife/gameOfLife.test.ts
+++ src/logic/gameOfLife/gameOfLife.test.ts
@@ -1,5 +1,10 @@
 import { computeNextGeneration } from "./gameOfLife";
+import type { Seed } from "./gameOfLife.types";
 
-test("helloGameOfLife returns hello", () => {
-  expect(computeNextGeneration()).toBe("hello");
+describe("computeNextGeneration", () => {
+  it("calculates underpopulation conditions for a single row with one cell", () => {
+    const seed: Seed = [[0, 1, 0]];
+    const result: Seed = computeNextGeneration(seed);
+    expect(result).toEqual([[0, 0, 0]]);
+  });
 });
--- src/logic/gameOfLife/gameOfLife.ts
+++ src/logic/gameOfLife/gameOfLife.ts
@@ -1,3 +1,5 @@
-export const computeNextGeneration = () => {
-  return "hello";
+import type { Seed } from "./gameOfLife.types";
+
+export const computeNextGeneration = (seed: Seed) => {
+  return [[0, 0, 0]];
 };
